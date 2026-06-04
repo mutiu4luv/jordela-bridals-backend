1278f5e94809193439fab139f7521bd6e675651c
mutiu4luv <chidiemmamadu@gmail.com>
2026-06-04 09:27:48 +0100

    res

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
-const app = require('./src/app')
-const connectDatabase = require('./src/config/database')
-const bootstrapAdmin = require('./src/config/bootstrapAdmin')
+const app = require("./src/app");
+const connectDatabase = require("./src/config/database");
+const bootstrapAdmin = require("./src/config/bootstrapAdmin");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Jodella backend!");
+});
 
 async function startServer() {
   try {
-    await connectDatabase()
-    await bootstrapAdmin()
+    await connectDatabase();
+    await bootstrapAdmin();
     app.listen(port, () => {
-      console.log(`Jodella backend listening on port ${port}`)
-    })
+      console.log(`Jodella backend listening on port ${port}`);
+    });
   } catch (error) {
-    console.error('Unable to start server:', error)
-    process.exit(1)
+    console.error("Unable to start server:", error);
+    process.exit(1);
   }
 }
 
-startServer()
+startServer();
